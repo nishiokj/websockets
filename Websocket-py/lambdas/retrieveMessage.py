e1d649e4e437abe5b135316a1bf9a2a27c838b34
Jevin Nishioka <nishiokj@umich.edu>
2022-08-01 19:10:13 -0500

    Working Demo

--- Websocket-py/lambdas/retrieveMessage.py
+++ Websocket-py/lambdas/retrieveMessage.py
@@ -1,16 +1,26 @@
 import json
-from layers.NotificationTable import Notification
+#from layers.NotificationTable import Notification
 from boto3.dynamodb.conditions import Attr
 import boto3
+import os
+import re
 def lambda_handler(event,context):
-    user_id = event["headers"].get("user_id")
-    notificationTable = Notification()
-    response = notificationTable.scan(FilterExpression=Attr("user_id").eq(user_id))
-    message = response["message"]
-    for i in message:
-        i = i.toUpper()
-
+    #user_id = event["headers"].get("user_id")
+    #match = re.search("table/(.)*",os.environ.get("arn:aws:dynamodb:us-east-1:264860945026:table/WebsocketTable"))
+    #table_name = match.group().split("/")[1].strip()
+    table_name = "WebsocketTable"
+    resource = boto3.resource('dynamodb',region_name="us-east-1")
+    table = resource.Table(table_name)
+    notificationTable = table
     connectionId = event["requestContext"]["connectionId"]
-    client = boto3.client('apigatewaymanagementapi',endpoint_url="https://19rp3f1jii.execute-api.us-east-1.amazonaws.com/production/@connections")
-    post = client.post_to_connection(ConnectionId = connectionId,Data=json.dumps(message).encode('utf-8'))
-    return {"StatusCode": 200}
+    response = notificationTable.scan(FilterExpression=Attr("user_id").eq(connectionId))
+    message = response["Items"][0]["message"]
+    print(response["Items"])
+    x = message.upper()
+    print(x)
+    
+    client = boto3.client('apigatewaymanagementapi',endpoint_url="https://19rp3f1jii.execute-api.us-east-1.amazonaws.com/production")
+    post = client.post_to_connection(ConnectionId = connectionId,Data=json.dumps(x).encode('utf-8'))
+    return {
+        "statusCode": 200
+        }
--- Websocket-py/lambdas/sendMessage.py
+++ Websocket-py/lambdas/sendMessage.py
@@ -1,22 +1,32 @@
-import json
+
 
 # import requests
-import datetime
+
 import os
 import boto3
-from layers.Connect import Connection
-from layers.NotificationTable import Notification
-from boto3.dynamodb.conditions import Attr
+import re
+
 
 def lambda_handler(event, context):
-    notificationTable = Notification()
-    user_id = event["headers"].get("userid")
+    #match = re.search("table/(.)*",os.environ.get("arn:aws:dynamodb:us-east-1:264860945026:table/WebsocketTable"))
+    #table_name = match.group().split("/")[1].strip()
+    resource = boto3.resource('dynamodb',region_name="us-east-1")
+    table_name = "WebsocketTable"
+    table = resource.Table(table_name)
+    notificationTable = table
+    #user_id = event["headers"].get("userid")
     connect = event["requestContext"]["connectionId"]
-    input = event["message"]
+    input = event["body"]
+    
+    phrase1 = input.rsplit(", ",2)
+    ans = ""
+    for i in phrase1[1]:
+        if i != '}':
+            ans += i
     notificationTable.put_item(Item={
-        'user_id' : user_id,
+        'user_id' : connect,
         'connection_id' : connect,
-        'message' : input
+        'message' : ans
     })
     return {
         "statusCode": 200
