6a856d91efc81de4629baaedf9a38d6962f9c632
Jevin Nishioka <nishiokj@umich.edu>
2022-08-03 10:11:32 -0500

    small changes

--- Websocket-py/lambdas/getSentiment.py
+++ Websocket-py/lambdas/getSentiment.py
@@ -1,17 +1,11 @@
-import json 
 
 import boto3
 
 def lambda_handler(event,context):
-    #call googleAPI
     comprehend = boto3.client(service_name='comprehend', region_name='us-east-1')
-    #event['message'] = "that guy is a clown and I do not like him"
     message = event['message']
-    #print(event)
     stringMessage = str(message)
-    #stringMessage = "I think that the current markets indicate that the stock market will go down. With Powell raising interest rates money will surely tighten in the coming months. This could be bad news for popular indexes"
     obj = comprehend.detect_sentiment(Text=stringMessage, LanguageCode='en')
-    
     sentimentScore = obj["SentimentScore"]
     Positive = sentimentScore['Positive']
     Negative = sentimentScore['Negative']
@@ -21,10 +15,10 @@ def lambda_handler(event,context):
     
     return {
         'Sentiment' : sentiment,
-        'Positive' : Positive,
-        'Negative' : Negative,
-        'Neutral' : Neutral,
-        'Mixed' : Mixed,
+        'PositiveConfidence' : Positive,
+        'NegativeConfidence' : Negative,
+        'NeutralConfidence' : Neutral,
+        'MixedConfidence' : Mixed,
         'statusCode': 200
         
     }
--- Websocket-py/lambdas/retrieveMessage.py
+++ Websocket-py/lambdas/retrieveMessage.py
@@ -2,31 +2,28 @@ import json
 #from layers.NotificationTable import Notification
 from boto3.dynamodb.conditions import Attr
 import boto3
-import os
-import re
+
+
 client1 = boto3.client('lambda')
 def lambda_handler(event,context):
-    #user_id = event["headers"].get("user_id")
-    #match = re.search("table/(.)*",os.environ.get("arn:aws:dynamodb:us-east-1:264860945026:table/WebsocketTable"))
-    #table_name = match.group().split("/")[1].strip()
+    #Make Dynamo table
     table_name = "WebsocketTable"
     resource = boto3.resource('dynamodb',region_name="us-east-1")
-    table = resource.Table(table_name)
-    notificationTable = table
+    notificationTable = resource.Table(table_name)
+    #Get message from connection
     connectionId = event["requestContext"]["connectionId"]
     response = notificationTable.scan(FilterExpression=Attr("user_id").eq(connectionId))
     message = response["Items"][0]["message"]
-    print(response["Items"])
-    newMessage = message.upper()
-    print(newMessage)
-    sentimentInput = {'message': newMessage}
+    #Invoke Sentiment Analysis
+    sentimentInput = {'message': message}
     response = client1.invoke(
         FunctionName='arn:aws:lambda:us-east-1:264860945026:function:Websocket-py-getSentimentLambda-X9JvFN2c6qZB',
         InvocationType='RequestResponse',
         Payload=json.dumps(sentimentInput),
     )
+    #Send back to client
     send_to_client = json.loads(response["Payload"].read())
-    send_to_client["OriginalMessage"] = newMessage
+    send_to_client["OriginalMessage"] = message
     client = boto3.client('apigatewaymanagementapi',endpoint_url="https://19rp3f1jii.execute-api.us-east-1.amazonaws.com/production")
     post = client.post_to_connection(ConnectionId = connectionId,Data=json.dumps(send_to_client).encode('utf-8'))
     return {
